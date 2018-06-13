cfa95edf0bb8de03ad63c6491064b01888b016ec
Andrew Mead <andrewjosephmead@gmail.com>
2018-06-13 14:15:51 -0400

    Change "Expensify" to "Budget App"

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({ startLogout }) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard">
-          <h1>Expensify</h1>
+          <h1>Budget App</h1>
         </Link>
         <button className="button button--link" onClick={startLogout}>Logout</button>
       </div>
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify</h1>
+      <h1 className="box-layout__title">Budget App</h1>
       <p>It's time to get your expenses under control.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
