c43938e13756eba088d4dda0d0db88b45f9c64bb
Andrew Mead <andrewjosephmead@gmail.com>
2018-09-20 17:00:51 -0400

    React v16 example

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,1 +1,6 @@
-require('dotenv').config({ path: '.env.test' });
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv'
+
+Enzyme.configure({ adapter: new Adapter() });
+dotenv.config({ path: '.env.test' });
